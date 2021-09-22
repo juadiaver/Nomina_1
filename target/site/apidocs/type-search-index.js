aae7e811eae4c49563f8d9201d6cf8cd94954acb
juanjose <juanjosediazvera@iessoterohernandez.es>
2021-09-22 10:52:30 +0200

    generado javadoc

---
+++ target/site/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"laboral","l":"CalculaNominas"},{"p":"laboral","l":"DatosNoCorrectosException"},{"p":"laboral","l":"Empleado"},{"p":"laboral","l":"Nomina"},{"p":"laboral","l":"Persona"}]
