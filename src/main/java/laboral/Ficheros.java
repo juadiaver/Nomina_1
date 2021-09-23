aed1a19da629c6931a80cbc8b51c312b138afc8d
juanjose <juanjosediazvera@iessoterohernandez.es>
2021-09-23 14:33:40 +0200

    Ejercicio 1 parte 2

--- src/main/java/laboral/Ficheros.java
+++ src/main/java/laboral/Ficheros.java
@@ -1,49 +1,82 @@
 package laboral;
 
+import java.util.*;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
-import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Ficheros {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
+        Nomina n = new Nomina();
+        FileReader fr = null;
+        BufferedReader br = null;
+        FileWriter fw = null;
+        BufferedWriter bw = null;
         File fichero = new File("Empleado.txt");
-        Scanner s = null;
+        List<Empleado> empleados = new ArrayList();
+        List lineaEmpleado = new ArrayList();
 
         try {
-            // Leemos el contenido del fichero
+            fr = new FileReader(fichero);
+            br = new BufferedReader(fr);
+            fw = new FileWriter(new File("Sueldo.txt"));
+            bw = new BufferedWriter(fw);
             
-            s = new Scanner(fichero);
-            String[] lineas = new String[5];
-            int n = 0;
-            // Leemos linea a linea el fichero
-            while (s.hasNextLine()) {
-                String linea = s.next();
-                lineas[n] = linea;
-                n ++;
+            while (br.ready()) {
+
+                String linea = br.readLine();
+                lineaEmpleado.add(linea);
 
             }
-            for (int i = 0; i < lineas.length; i++) {
-                System.out.println(lineas[i]);
-            }
-            int antiguedad= Integer.parseInt(lineas[3]);
-            int categoria= Integer.parseInt(lineas[4]);
-            String nombre= lineas[0];
-            String dni= lineas[1];
-            String sexo= lineas[2];
-            Empleado a = new Empleado(antiguedad,categoria,nombre,dni,sexo.charAt(0));
-            a.imprime();
 
-        } catch (Exception ex) {
+            Iterator iter = lineaEmpleado.iterator();
+
+            while (iter.hasNext()) {
+
+                String string = (String) iter.next();
+                String[] parts = string.split(",");
+                String nombre = parts[0];
+                String dni = parts[1];
+                String sexo = parts[2];
+                int antiguedad = Integer.parseInt(parts[3]);
+                int categoria = Integer.parseInt(parts[4]);
+
+                Empleado a = new Empleado(antiguedad, categoria, nombre, dni, sexo.charAt(0));
+                empleados.add(a);
+
+            }
+            for (Empleado emp : empleados) {
+                int sueldo = n.sueldo(emp);
+                bw.write("Dni: "+emp.dni+" Sueldo: "+sueldo);
+                bw.newLine();
+
+            }
+
+        } catch (IOException ex) {
             System.out.println("Mensaje: " + ex.getMessage());
-        } finally {
-            // Cerramos el fichero tanto si la lectura ha sido correcta o no
+        } catch (DatosNoCorrectosException ex) {
+            System.out.println("Mensaje: " + ex.getMessage());
+        }
+
+        finally {
             try {
-                if (s != null)
-                    s.close();
-            } catch (Exception ex2) {
-                System.out.println("Mensaje 2: " + ex2.getMessage());
+                if (br != null)
+                    br.close();
+                if (fr != null)
+                    fr.close();
+                if (bw != null)
+                    bw.close();
+                if (fw != null)
+                    fw.close();
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
             }
         }
+
     }
 }
